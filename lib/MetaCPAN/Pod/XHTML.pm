1c241becbad36880a5dc417e8e16e133f9b51064
Olaf Alders <olaf@wundersolutions.com>
2011-01-07 18:07:56 -0600

    Fixes issue #8 (incorrect in-page linking)

--- lib/MetaCPAN/Pod/XHTML.pm
+++ lib/MetaCPAN/Pod/XHTML.pm
@@ -14,15 +14,15 @@ sub start_L {
     my ( $self, $flags ) = @_;
     my ( $type, $to, $section ) = @{$flags}{ 'type', 'to', 'section' };
 
-    #print "$type $to $section\n" if $section;
-
     my $url
         = $type eq 'url' ? $to
         : $type eq 'pod' ? $self->resolve_pod_page_link( $to, $section )
         : $type eq 'man' ? $self->resolve_man_page_link( $to, $section )
         :                  undef;
 
-    my $class = ( $type eq 'pod' ) ? ' class="moduleLink"' : '';
+    my $pound = '#';
+    my $class
+        = ( $type eq 'pod' && ($url !~ m{$pound}) ) ? ' class="moduleLink"' : '';
 
     $self->{'scratch'} .= qq[<a href="$url"$class>];
 }
@@ -55,3 +55,4 @@ Override default behaviour by doing nothing.
 Wrap code snippets in <pre> tags for easier syntax highlighting.
 
 =cut
+
